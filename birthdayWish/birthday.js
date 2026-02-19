651bca30812679dd863abe109a82a26847132613
Muskaan <hasratnabiha@gmail.com>
2026-02-19 13:10:59 +0530

    Add some transitions

--- birthdayWish/birthday.js
+++ birthdayWish/birthday.js
@@ -1,7 +1,3 @@
-const startBtn = document.getElementById("startBtn");
-const startScreen = document.getElementById("startScreen");
-
-openEnvelope();
 function openEnvelope(){
 let envelop = document.querySelector('.envelop');
 let stamp = document.querySelector('.stamp');
@@ -10,18 +6,22 @@ let replay = document.querySelector('.replay');
 
 console.log('worked');
 envelop.style.opacity = '1';
-envelop.addEventListener('click',function(){
-    envelop.style.height = "500px";
-    envelop.style.top = "15%";
-    envelop.style.clipPath =
-    "polygon(50% 0%, 100% 20%, 100% 100%, 0 100%, 0 20%)";
-    
-    stamp.style.transform = 'translate(-25%, -45%)';
 
-    msg.style.opacity = '1';
-    typeMessage();
-    launchConfetti();
-})
+envelop.addEventListener("click", () => {
+    stamp.classList.add("move");
+    setTimeout(() => {
+      envelop.style.height = "500px";
+      envelop.style.top = "15%";
+      envelop.style.clipPath =
+      "polygon(50% 0%, 100% 20%, 100% 100%, 0 100%, 0 20%)";
+    },50);
+  
+    setTimeout(() => {
+        msg.classList.add("show");
+        typeMessage();
+        launchConfetti();
+    }, 700);
+});
 
 // Typewriter effect
 function typeMessage() {
@@ -40,29 +40,22 @@ function typeMessage() {
       clearInterval(interval);
       replay.style.opacity = '1';
     }
-  }, 40);
+  }, 20);
 }
 
 function launchConfetti() {
   const emojis = ["🎉", "🎂", "🎈", "🥳"];
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 100; i++) {
     const confetti = document.createElement("div");
     confetti.className = "confetti";
     confetti.innerText = emojis[Math.floor(Math.random() * emojis.length)];
     confetti.style.left = Math.random() * 100 + "vw";
     confetti.style.animationDuration = 2 + Math.random() * 2 + "s";
     document.body.appendChild(confetti);
-
-    setTimeout(() => confetti.remove(), 4000);
+    setTimeout(()=>confetti.remove(),15000);
   }
 }
 }
 
-
-
-
-
-
-
-
+openEnvelope();
 
